0be3e3771633e79d60785ce9b03dcfa0dcb25b32
Nia Bert <nbert14@gmail.com>
2019-08-05 01:20:29 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,28 +1,29 @@
 class Show < ActiveRecord::Base
   
-def Show::highest_rating
-  Show.maximum("rating")
-end
-
-def Show::most_popular_show
-  Show.order(rating: :desc).first
-end 
-
-def Show::lowest_rating
-  Show.minimum("rating")
-end
-
-def Show::least_popular_show
-  Show.order(rating: :asc).first
-end 
-
-def Show::ratings_sum
-end
-
-def popular_shows
-end
-
-def shows_by_alphabetical_order
-end
+  def Show::highest_rating
+    Show.maximum("rating")
+  end
+  
+  def Show::most_popular_show
+    Show.order(rating: :desc).first
+  end 
+  
+  def Show::lowest_rating
+    Show.minimum("rating")
+  end
+  
+  def Show::least_popular_show
+    Show.order(rating: :asc).first
+  end 
+  
+  def Show::ratings_sum
+    Show.sum("rating")
+  end
+  
+  def Show::popular_shows
+  end
+  
+  def Show::shows_by_alphabetical_order
+  end
 
 end
