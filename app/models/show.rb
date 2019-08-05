5b45c362fe93169096e977b1e0f488fcb407f36f
Nia Bert <nbert14@gmail.com>
2019-08-05 01:12:36 +0000

    Automatically backed up by Learn

---
+++ app/models/show.rb
@@ -0,0 +1,27 @@
+class Show < ActiveRecord::Base
+  
+def Show::highest_rating
+  Show.maximum("rating")
+end
+
+def Show::most_popular_show
+  Show.order(rating: :desc).first
+end 
+
+def Show::lowest_rating
+  Show.minimum("rating")
+end
+
+def Show::least_popular_show
+end 
+
+def Show::ratings_sum
+end
+
+def popular_shows
+end
+
+def shows_by_alphabetical_order
+end
+
+end
